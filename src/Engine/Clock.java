16ed305bdcc518fd3a53caa31ac103b027410808
Dzmitry Ananchuk <psyhogenesis@gmail.com>
2015-02-07 15:16:12 +0100

    zegar, zmiana trybu 12/24, data (poczatek)

--- src/Engine/Clock.java
+++ src/Engine/Clock.java
@@ -1,6 +1,22 @@
 package Engine;
 
+import java.util.Calendar;
+
 public class Clock
 {
-
+	public showTime(){
+	Calendar curTime = Calendar.getInstance();
+	curTime.setTimeInMillis(System.currentTimeMillis());
+    double hour = curTime.get(Calendar.HOUR_OF_DAY);
+    double min = curTime.get(Calendar.MINUTE);
+    double sec = curTime.get(Calendar.SECOND);
+    
+    return hour + ":" + min + " " + sec;
+	}
+	
+	public change24to12(){
+		if(Integer.parseInt(hour) >= 12.0)
+			hour -= 12;
+	}
+	}
 }
--- src/Engine/Date.java
+++ src/Engine/Date.java
@@ -2,5 +2,8 @@ package Engine;
 
 public class Date
 {
-
+	public String showDate(){
+		Date date = new Date();
+		return null;
+	}
 }
