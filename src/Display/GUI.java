c7f06ae1afa9fd74f4cfbbd0c5674d36d6b2e507
Paweł Mieleń <lierox90@gmail.com>
2015-02-11 21:25:50 +0100

    Okienko

--- src/Display/GUI.java
+++ src/Display/GUI.java
@@ -1,10 +1,26 @@
 package Display;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 public class GUI extends JFrame implements Runnable
 {
-
+	//Inits
+	private int width=300;
+	private int heigh=200;
+	//Constructor
+    public GUI() throws IOException 
+    {
+    	//Window init
+        super("TheClock");
+        setSize(width, heigh);
+        setResizable( false );
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+        setLocationRelativeTo(null);
+    }
 	@Override
 	public void run()
 	{
