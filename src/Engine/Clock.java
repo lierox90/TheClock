46345224a38636d8be80ec2f7a6a8c1486d3dda6
Dzmitry Ananchuk <psyhogenesis@gmail.com>
2015-02-11 21:08:45 +0100

    Data+Clock

--- src/Engine/Clock.java
+++ src/Engine/Clock.java
@@ -1,18 +1,53 @@
 package Engine;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.Locale;
+
+import javax.swing.Timer;
+
 public class Clock implements Runnable
 {
-	private int hours;
-	private int minuts;
-	private int seconds;
+	private int hours, minutes, seconds;
+	private String today;
+	private Calendar calendar;
+	private Formatter fmt;
 	
 	
 
 	@Override
-	public void run()
+	public void run() 
 	{
-		// TODO Auto-generated method stub
 		
+		seconds++;
+		if(seconds == 60){ 
+	      seconds = 0; 
+	      minutes++; 
+		  if(minutes == 60){ 
+	        minutes = 0;
+	        hours++;		
+		  }	
+	      if(hours == 24){ 
+	        hours = 0;
+	      }		 
+		}
 	}
-
+	
+	  public String twelveHourFormat(){
+	    fmt = new Formatter();
+	    calendar = Calendar.getInstance();
+		today = fmt.format("%tr",calendar).toString();
+		return today;
+	  }
+	  
+	  public String twentyFourthHourFormat(){
+	    fmt = new Formatter();
+	    calendar = Calendar.getInstance();
+		today = fmt.format("%tT",calendar).toString();
+		return today;
+	  }
 }
--- src/Engine/Date.java
+++ src/Engine/Date.java
@@ -1,7 +1,17 @@
 package Engine;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Formatter;
+
 public class Date implements Runnable
 {
+	
+	private Calendar calendar;
+	private SimpleDateFormat formatter;
+	private Date currentDate;
+	private Formatter fmt;
 
 	@Override
 	public void run()
@@ -9,5 +19,10 @@ public class Date implements Runnable
 		// TODO Auto-generated method stub
 		
 	}
+	public String getFullDate(){
+	    fmt = new Formatter();
+		calendar = Calendar.getInstance();
+		return fmt.format("%1$tA %1$td %1$tB %1$tY",calendar).toString();
+	  }
 
 }
