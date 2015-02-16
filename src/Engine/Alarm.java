89fc8a1b851a38e72817aafbdb8548c59a19eed7
Paweł Mieleń <lierox90@gmail.com>
2015-02-16 20:09:15 +0100

    wykomentowanie w alarmie zeby nie bylo bledu

--- src/Engine/Alarm.java
+++ src/Engine/Alarm.java
@@ -8,18 +8,22 @@ public class Alarm implements Runnable
 	@Override
 	public void run()
 	{
-		for(;;){
-			if(/*alarm jest wlaczony*/){
-			    String currentTime = /*Clock.getInstance()*/;
-		        String signalTime = /*czas na zegarku alarmowym*/;
-				if(signalTime.equals(currentTime)){ 
-					for(int i=0;i<10;i++){		
+		for(;;)
+		{
+			/*if(alarm jest wlaczony)
+			{
+			    String currentTime = Clock.getInstance();
+		        String signalTime = czas na zegarku alarmowym;
+				if(signalTime.equals(currentTime))
+				{ 
+					for(int i=0;i<10;i++)
+					{		
 						Toolkit.getDefaultToolkit().beep();
 						Thread.sleep(300);
-					  }
+					}
 				    break;		
-				  }
-		      }
+				}
+		    }*/
 		}
 	}
 }
