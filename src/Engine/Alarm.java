ee5dd3a088216fd7d8d5cbcbd49a1862d04cb7e8
Dzmitry Ananchuk <psyhogenesis@gmail.com>
2015-02-16 11:54:56 +0100

    Alarm upd1

--- src/Engine/Alarm.java
+++ src/Engine/Alarm.java
@@ -1,11 +1,25 @@
 package Engine;
 
+import java.awt.Toolkit;
+
 public class Alarm implements Runnable
 {
 
 	@Override
 	public void run()
 	{
-		
+		for(;;){
+			if(/*alarm jest wlaczony*/){
+			    String currentTime = /*Clock.getInstance()*/;
+		        String signalTime = /*czas na zegarku alarmowym*/;
+				if(signalTime.equals(currentTime)){ 
+					for(int i=0;i<10;i++){		
+						Toolkit.getDefaultToolkit().beep();
+						Thread.sleep(300);
+					  }
+				    break;		
+				  }
+		      }
+		}
 	}
 }
