d37ad484b2835101512b08e61fa7c63b22ecedcf
Paweł Mieleń <lierox90@gmail.com>
2015-02-18 16:39:21 +0100

    Formatowanie + klasa na 2 zegarek

--- src/Engine/Stoper.java
+++ src/Engine/Stoper.java
@@ -14,41 +14,49 @@ class Stoper implements Runnable
 	}
 
 
-	  private void start() {
-	    this.startTime = System.currentTimeMillis();
-	    this.running = true;
+	  private void start() 
+	  {
+		  this.startTime = System.currentTimeMillis();
+		  this.running = true;
 	  }
 
 
-	  private void stop() {
-	    this.stopTime = System.currentTimeMillis();
-	    this.running = false;
+	  private void stop() 
+	  {
+		  this.stopTime = System.currentTimeMillis();
+		  this.running = false;
 	  }
 
 
 	  //czas ktory minal z 1/100 milisekunda
-	  private long getElapsedTime() {
-	    long elapsed;
-	    if (running) {
-	      elapsed = ((System.currentTimeMillis() - startTime) / 10);
-	    }
-	    else {
-	      elapsed = (stopTime - startTime);
-	    }
-	    return elapsed;
+	  private long getElapsedTime()
+	  {
+		  long elapsed;
+		  if (running) 
+		  {
+			  elapsed = ((System.currentTimeMillis() - startTime) / 10);
+		  }
+		  else 
+		  {
+			  elapsed = (stopTime - startTime);
+		  }
+		  return elapsed;
 	  }
 
 
 	  //czas ktory minal z krokiem w 1 sekunde
-	  private long getElapsedTimeSecs() {
-	    long elapsed;
-	    if (running) {
-	      elapsed = ((System.currentTimeMillis() - startTime) / 1000);
-	    }
-	    else {
-	      elapsed = ((stopTime - startTime) / 1000);
-	    }
-	    return elapsed;
+	  private long getElapsedTimeSecs() 
+	  {
+		  long elapsed;
+		  if (running) 
+		  {
+			  elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+		  }
+		  else 
+		  {
+			  elapsed = ((stopTime - startTime) / 1000);
+	      }
+		  return elapsed;
 	  }
 	
 	
