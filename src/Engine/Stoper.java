c10dea888afe0e49e1f43a05e6b9b6777c0374be
Paweł Mieleń <lierox90@gmail.com>
2015-02-18 16:42:26 +0100

    Formatowanie x2

--- src/Engine/Stoper.java
+++ src/Engine/Stoper.java
@@ -9,56 +9,49 @@ class Stoper implements Runnable
 	@Override
 	public void run()
 	{
-		// TODO Auto-generated method stub
 		
 	}
-
-
-	  private void start() 
-	  {
-		  this.startTime = System.currentTimeMillis();
-		  this.running = true;
-	  }
-
-
-	  private void stop() 
-	  {
-		  this.stopTime = System.currentTimeMillis();
-		  this.running = false;
-	  }
-
-
-	  //czas ktory minal z 1/100 milisekunda
-	  private long getElapsedTime()
-	  {
-		  long elapsed;
-		  if (running) 
-		  {
-			  elapsed = ((System.currentTimeMillis() - startTime) / 10);
-		  }
-		  else 
-		  {
-			  elapsed = (stopTime - startTime);
-		  }
-		  return elapsed;
-	  }
-
-
-	  //czas ktory minal z krokiem w 1 sekunde
-	  private long getElapsedTimeSecs() 
-	  {
-		  long elapsed;
-		  if (running) 
-		  {
-			  elapsed = ((System.currentTimeMillis() - startTime) / 1000);
-		  }
-		  else 
-		  {
-			  elapsed = ((stopTime - startTime) / 1000);
-	      }
-		  return elapsed;
-	  }
 	
+	private void start() 
+	{
+		this.startTime = System.currentTimeMillis();
+		this.running = true;
+	}
+
+	private void stop() 
+	{
+		this.stopTime = System.currentTimeMillis();
+		this.running = false;
+	}
 	
+	//czas ktory minal z 1/100 milisekunda
+	private long getElapsedTime()
+	{
+		long elapsed;
+		if (running) 
+		{
+			elapsed = ((System.currentTimeMillis() - startTime) / 10);
+		}
+		else 
+		{
+			elapsed = (stopTime - startTime) / 10;
+		}
+		return elapsed;
+	}
+
+	//czas ktory minal z krokiem w 1 sekunde
+	private long getElapsedTimeSecs() 
+	{
+		long elapsed;
+	  	if (running) 
+	  	{
+	  		elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+	  	}
+	  	else 
+	  	{
+	  		elapsed = ((stopTime - startTime) / 1000);
+	  	}
+	  	return elapsed;
+	}
 }
 
